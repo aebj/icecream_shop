8ab98488a869b6b7868160740695c7eb2abcb7e8
celu29462 <73479501+celu29462@users.noreply.github.com>
2021-05-11 10:20:59 +0200

    Update index.php
    add form

--- index.php
+++ index.php
@@ -6,9 +6,20 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Icecream Shop</title>
   </head>
   <body>
-    
+    <form action="/action_page.php">
+      <label for="bar_code">Bar code:</label><br>
+      <input type="text" id="bar_code" name="bar_code" value="Bar code"><br>
+
+      <label for="stock">Stock:</label><br>
+      <input type="number" id="stock" name="stock" value="Stock"><br><br>
+
+      <label for="expiration_date">Expiration date:</label><br>
+      <input type="date" id="expiration_date" name="expiration_date" value="yyyy-mm-dd"><br><br>
+
+      <input type="submit" value="Submit">
+    </form>
   </body>
 </html>
